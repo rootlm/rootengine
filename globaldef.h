328aea025701cf35d9ebdbd288f00179f14490fd
rootlm <liammillay2@gmail.com>
2017-02-25 23:14:40 -0500

    Add files via upload

---
+++ globaldef.h
@@ -0,0 +1,16 @@
+#ifndef __GLOBALDEF__
+#define __GLOBALDEF__
+//these are global defines
+
+//Screen dimension constants
+#define SCREEN_WIDTH 640
+#define SCREEN_HEIGHT 480
+
+#define APPTITLE "root engine"
+
+//limits
+//These define the size of the arrays that hold stuff
+#define MAXOBJECTS 512
+//There can't be any more sprites defined than this so CHANGE IT WHEN THE GAME HAS TOO MANY
+#define MAXSPRITES 32
+#endif
